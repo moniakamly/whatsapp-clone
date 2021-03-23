b8176d08bafff21ddc95ef7268d698ed989957ca
AYMEN <moniakamli95@gmail.com>
2021-03-23 15:28:29 +0100

    congiguration of firebase

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAkpu0rAkAoQww0DUbr71XRrnK_olwuz-c",
+    authDomain: "whatsapp-mern-47036.firebaseapp.com",
+    projectId: "whatsapp-mern-47036",
+    storageBucket: "whatsapp-mern-47036.appspot.com",
+    messagingSenderId: "584578118109",
+    appId: "1:584578118109:web:7b4b4482d2d98a4e81c634",
+    measurementId: "G-NLGV0MBTN7"
+  };
